c4f2a4e9aba3fd93fe0df291c12d1c68fb496a83
Alex Brandao <alex12a7x@gmail.com>
2021-09-05 11:05:36 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+function soma(a,b){
+    return a+b
+}
+function sub(a,b){
+    return a-b
+}
+function div(a,b){
+    return a/b
+}
+function multi(a,b){
+    return a*b
+}
+
+console.log(soma(5,1))
