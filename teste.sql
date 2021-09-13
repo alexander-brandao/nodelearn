b7ab36898abb8eb1bdeac52a22d4ec7f40f57e4a
Alex Brandao <alex12a7x@gmail.com>
2021-09-13 16:36:35 -0300

    inserindo dados na tabela

--- teste.sql
+++ teste.sql
@@ -1,5 +1,13 @@
+/* criando tabela no banco de dados */
 create table usuarios(
     nome varchar(50),
     email varchar(100),
     idade int
 );
+
+/*inserindo dados na tablela usuarios */
+Insert into usuarios(nome, email, idade) values(
+    "Alexander Brandao",
+    "email@gmail.com",
+    25
+):
