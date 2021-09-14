7038715ccf83fcea72b0be72c735ea5b68df7e22
Alex Brandao <alex12a7x@gmail.com>
2021-09-14 20:50:43 -0300

    testando servidor

---
+++ sequelize_servidor.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+
+// criando um objeto para guardar um banco de dados
+// prametros test:nome do banco, root: nome padrao usuario, s, objeto JSON
+const sequelize = new Sequelize('test', 'root', 't', {
+   //qual maquina esta o servidor? - localhost = meu proprio computador
+    host: "localhost",
+    // dialect: linguagem de database que o sequelize vai utilizar "mysql"
+    dialect: 'mysql'
+})
+
+//programacao assincrona 
+sequelize.authenticate().then(function(){
+    console.log("concectado com sucesso!")
+}).catch(function(erro){
+    console.log("falha ao se conectar: " + erro)
+})
